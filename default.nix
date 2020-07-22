2e4cce66c15bc0bbe172eb2939e1cb709a099e01
jD91mZM2 <me@krake.one>
2020-07-22 18:05:51 +0200

    Pin python 3.7 in default.nix as well

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@
 let
   overrides = import ./overrides.nix { inherit pkgs; };
 in pkgs.poetry2nix.mkPoetryApplication {
+  python = pkgs.python37;
   projectDir = ./.;
   overrides = pkgs.poetry2nix.overrides.withDefaults overrides;
 }
